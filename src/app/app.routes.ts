8127c465362719d39c4a0b8da97a7502fe40ec4f
poomipat-k <k.poomipat@gmail.com>
2025-05-22 14:46:59 +0700

    list setup basic path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,19 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { ListPageComponent } from './list-page/list-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent,
-    title: 'Ice and Fire Home page',
-  },
-  {
     path: 'list',
     component: ListPageComponent,
-    title: 'Ice and Fire Home page',
+    title: 'List page',
   },
+  {
+    path: '',
+    // component: HomeComponent,
+    title: 'Ice and Fire Home page',
+    redirectTo: '/list',
+    pathMatch: 'full',
+  },
+  { path: '**', component: PageNotFoundComponent },
 ];
