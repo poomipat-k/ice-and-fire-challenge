d21e74a83eb6699bbd70154b75c67ebbf15fa313
poomipat-k <k.poomipat@gmail.com>
2025-05-22 14:17:29 +0700

    init list page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListPageComponent } from './list-page/list-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Ice and Fire Home page',
+  },
+  {
+    path: 'list',
+    component: ListPageComponent,
+    title: 'Ice and Fire Home page',
+  },
+];
---
+++ src/app/components/nav-link-button/nav-link-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-link-button',
+  imports: [],
+  templateUrl: './nav-link-button.component.html',
+  styleUrl: './nav-link-button.component.scss'
+})
+export class NavLinkButtonComponent {
+
+}
