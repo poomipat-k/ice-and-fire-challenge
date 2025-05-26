64f9f0eab3db915287680084c5bfdbeca961d40c
poomipat-k <k.poomipat@gmail.com>
2025-05-27 01:28:37 +0700

    details basic character details page

--- src/app/details-character/details-character.component.ts
+++ src/app/details-character/details-character.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+  OnInit,
+} from '@angular/core';
+import { CharacterDetailsStore } from './character-details.store';
 
 @Component({
   selector: 'app-details-character',
   imports: [],
   templateUrl: './details-character.component.html',
-  styleUrl: './details-character.component.scss'
+  styleUrl: './details-character.component.scss',
+  providers: [CharacterDetailsStore],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DetailsCharacterComponent {
+export class DetailsCharacterComponent implements OnInit {
+  protected readonly characterId = input<string>(); // path param
+  readonly store = inject(CharacterDetailsStore);
 
+  ngOnInit(): void {
+    const id = Number(this.characterId());
+    this.store.loadById(id);
+  }
 }
