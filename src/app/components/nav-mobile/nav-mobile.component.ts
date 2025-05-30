ea2b9e1fbe4584ce53bf795b17bd1830179171d5
poomipat-k <k.poomipat@gmail.com>
2025-05-30 13:25:32 +0700

    fav finish mobile nav

--- src/app/components/nav-mobile/nav-mobile.component.ts
+++ src/app/components/nav-mobile/nav-mobile.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { IronThroneComponent } from '../svg/iron-throne/iron-throne.component';
 import { NavMobileStore } from './nav-mobile.store';
 
 @Component({
   selector: 'app-com-nav-mobile',
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, IronThroneComponent],
   templateUrl: './nav-mobile.component.html',
   styleUrl: './nav-mobile.component.scss',
   providers: [NavMobileStore],
